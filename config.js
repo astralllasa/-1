dfc2a022e40e1b68ff09fbb547abd535a4acf7a2
astralllasa <shengcy2000@gmail.com>
2026-02-12 22:06:09 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,63 @@
+// config.js
+window.forumConfig = {
+    // 帖子 2
+    "orange_party": {
+        title: "「公告」系统正在维护中，有一些帖子数据丢失，无法点击，请稍安勿躁。",
+        opName: "镇长",
+        replyCount: 1,
+        isFake: true 
+    }, // <--- 这里加上了逗号！
+
+        "dog_issue": {
+        title: "「求助」我的狗总是对邻居的狗进行骑跨行为，对方主人每次都吓得尖叫，我该怎么道歉？",
+        opName: "我家狗狗可爱",
+        totalPages: 8,
+        replyCount: 267,
+        isLocked: true
+    }, // 逗号不能忘
+
+    // 帖子 3
+    "man_issue": {
+        title: "怎么才能在有好感的人面前停止发疯",
+        opName: "混吃等死123",
+        totalPages: 1,
+        replyCount: 12,
+        isLocked: true
+    },
+
+    // === 这是一个假帖子 ===
+    "ad_post": {
+        title: "我妈总在群里发我小时候的尴尬照，我快被气疯了",
+        opName: "汤姆狸",
+        replyCount: 20,
+        isFake: true  // <--- 加上这一行，表示它是假的
+    },
+
+       "fake3": {
+        title: " 太好了我磕的cp是真的…",
+        opName: "鱼丸火锅",
+        totalPages: 1,
+    },
+    
+    "fake1": {
+        title: "有没有啥早餐推荐",
+        opName: "Andy12hsu",
+        replyCount: 10,
+        isFake: true 
+    },
+
+     "fake2": {
+        title: "我家鹦鹉学会骂人了",
+        opName: "Sakkkk",
+        replyCount: 2,
+        isFake: true 
+    },
+    "fake4": {
+        title: " 救命我感觉我兄弟对我有意思",
+        opName: "十万火急",
+        totalPages: 1,
+        isFake: true 
+    },
+
+};
+
