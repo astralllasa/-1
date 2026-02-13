79bd77ce9041f37d6ffbf4d421e9025a42a79a70
astralllasa <shengcy2000@gmail.com>
2026-02-13 23:28:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // config.js
 window.forumConfig = {
-    // 帖子 2
+
     "orange_party": {
         title: "「公告」系统正在维护中，有一些帖子数据丢失，无法点击，请稍安勿躁。",
         opName: "镇长",
         replyCount: 1,
         isFake: true 
-    }, // <--- 这里加上了逗号！
+    }, 
 
         "dog_issue": {
         title: "「求助」我的狗总是对邻居的狗进行骑跨行为，对方主人每次都吓得尖叫，我该怎么道歉？",
@@ -14,9 +14,9 @@ window.forumConfig = {
         totalPages: 8,
         replyCount: 267,
         isLocked: true
-    }, // 逗号不能忘
+    }, 
 
-    // 帖子 3
+
     "man_issue": {
         title: "怎么才能在有好感的人面前停止发疯",
         opName: "混吃等死123",
@@ -46,19 +46,21 @@ window.forumConfig = {
         totalPages: 1,
     },
     
-    "fake4": {
-        title: " 救命我感觉我兄弟对我有意思",
+      "fake4": {
+        title: "救命我感觉我兄弟对我有意思",
         opName: "十万火急",
         totalPages: 1,
-        isFake: true 
+        replyCount: 9,
+        isLocked: true
     },
+
 
         // === 这是一个假帖子 ===
     "ad_post": {
         title: "我妈总在群里发我小时候的尴尬照，我快被气疯了",
         opName: "汤姆狸",
         replyCount: 20,
-        isFake: true  // <--- 加上这一行，表示它是假的
+        isFake: true  
     },
 
 };
